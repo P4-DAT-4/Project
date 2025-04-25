a9f2fbf8931961aeabfae46081d2d78718b07d9c
Jacob Højlund Simonsen <jacobhoejlund@gmail.com>
2025-04-25 23:11:23 +0200

    DPP-138 Implement fields in stmt nodes matching abstract syntax

--- AFS/astbuilder/nodes/stmt/StmtAssignmentNode.java
+++ AFS/astbuilder/nodes/stmt/StmtAssignmentNode.java
@@ -1,10 +1,33 @@
 package astbuilder.nodes.stmt;
 
+import astbuilder.nodes.expr.ExprNode;
 import astbuilder.visitor.StmtVisitor;
 
 public class StmtAssignmentNode extends StmtNode {
+    private final String identifier;
+    private final ExprNode expression;
+
+    public StmtAssignmentNode(String identifier, ExprNode expression)
+    {
+        this.identifier = identifier;
+        this.expression = expression;
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public ExprNode getExpression() {
+        return expression;
+    }
+
     @Override
     public void acceptVisit(StmtVisitor visitor) {
         visitor.visitStmtAssignmentNode(this);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("StmtAssignment (%s = %s)", identifier, expression);
     }
 }
--- AFS/astbuilder/nodes/stmt/StmtCompositionNode.java
+++ AFS/astbuilder/nodes/stmt/StmtCompositionNode.java
@@ -3,8 +3,29 @@ package astbuilder.nodes.stmt;
 import astbuilder.visitor.StmtVisitor;
 
 public class StmtCompositionNode extends StmtNode {
+    private final StmtNode leftStatement;
+    private final StmtNode rightStatement;
+
+    public StmtCompositionNode(StmtNode leftStatement, StmtNode rightStatement) {
+        this.leftStatement = leftStatement;
+        this.rightStatement = rightStatement;
+    }
+
+    public StmtNode getLeftStatement() {
+        return leftStatement;
+    }
+
+    public StmtNode getRightStatement() {
+        return rightStatement;
+    }
+
     @Override
     public void acceptVisit(StmtVisitor visitor) {
         visitor.visitStmtCompositionNode(this);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("StmtComposition (%s %s)", leftStatement, rightStatement);
     }
 }
