94948b25152318aa974b1d716a80035801b67d90
Jacob Højlund Simonsen <jacobhoejlund@gmail.com>
2025-04-25 22:55:38 +0200

    DPP-138 Implement fields in event nodes matching abtstract syntax tree

--- AFS/astbuilder/nodes/event/EventDeclarationNode.java
+++ AFS/astbuilder/nodes/event/EventDeclarationNode.java
@@ -1,8 +1,28 @@
 package astbuilder.nodes.event;
 
+import astbuilder.nodes.expr.ExprFunctionCallNode;
+import astbuilder.nodes.expr.ExprNode;
 import astbuilder.visitor.EventVisitor;
 
+import java.util.List;
+
 public class EventDeclarationNode extends EventNode {
+    private final ExprNode expression;
+    private final ExprFunctionCallNode functionCall;
+
+    public EventDeclarationNode(ExprNode expression, ExprFunctionCallNode functionCall) {
+        this.expression = expression;
+        this.functionCall = functionCall;
+    }
+
+    public ExprNode getExpression() {
+        return expression;
+    }
+
+    public ExprFunctionCallNode getFunctionCall() {
+        return functionCall;
+    }
+
     @Override
     public void acceptVisit(EventVisitor visitor) {
         visitor.visitEventDeclarationNode(this);
--- AFS/astbuilder/nodes/expr/ExprFunctionCallNode.java
+++ AFS/astbuilder/nodes/expr/ExprFunctionCallNode.java
@@ -2,7 +2,25 @@ package astbuilder.nodes.expr;
 
 import astbuilder.visitor.ExprVisitor;
 
+import java.util.List;
+
 public class ExprFunctionCallNode extends ExprNode {
+    private final String identifier;
+    private final List<ExprNode> expressions;
+
+    public ExprFunctionCallNode(String identifier, List<ExprNode> expressions) {
+        this.identifier = identifier;
+        this.expressions = expressions;
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public List<ExprNode> getExpressions() {
+        return expressions;
+    }
+
     @Override
     public void acceptVisit(ExprVisitor visitor) {
         visitor.visitExprFunctionCallNode(this);
