c06618e7a6de195e1fe8d4d951c8598c37b0644c
Kristoffer <XA68OU@student.aau.dk>
2025-05-06 11:48:56 +0200

    Ikke helt færdig

--- AFS/afs/astbuilder/checker/test/TypeCheckerTest.java
+++ AFS/afs/astbuilder/checker/test/TypeCheckerTest.java
@@ -1,5 +1,54 @@
 package afs.astbuilder.checker.test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+// Uses Junit5 for unit testing TypeChecker
 public class TypeCheckerTest {
+  @Test
+  public void validateDeclaration() {
   
+  }
+  @Test
+  public void validateEvent() {
+
+  }
+  @Test
+  public void validateStatement() {
+
+  }
+  @Test
+  public void validateExpression() {
+    @Nested
+    class Binop {
+      @Test
+      public void validateBinop() {
+        TypeChecker typeChecker = new TypeChecker();
+        String input = "validInput"
+
+        String result = typeChecker.check(input);
+
+        assertEquals("ExpectedOutput", result);
+      }
+    }
+    @Nested
+    class Unop {
+      @Test
+      public void validateUnop() {
+        TypeChecker typeChecker = new TypeChecker();
+        String input = "validInput"
+
+        String result = typeChecker.check(input);
+
+        assertEquals("ExpectedOutput", result);
+      }
+    }
+    
+  } 
+  @Test
+  public void validateTypeType() {
+
+  }
 }
