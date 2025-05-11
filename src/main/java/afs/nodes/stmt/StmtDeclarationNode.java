08332d48ba6011886400c41e8ab32aaa175b3f0c
Jacob Højlund Simonsen <jacobhoejlund@gmail.com>
2025-05-11 19:45:00 +0200

    DPP-142 Add type environment

---
+++ src/main/java/afs/nodes/stmt/StmtDeclarationNode.java
@@ -0,0 +1,40 @@
+package afs.nodes.stmt;
+
+import afs.nodes.expr.ExprNode;
+import afs.nodes.type.TypeNode;
+
+public final class StmtDeclarationNode extends StmtNode {
+    private final TypeNode type;
+    private final String identifier;
+    private final ExprNode expression;
+    private final StmtNode statement;
+
+    public StmtDeclarationNode(TypeNode type, String identifier, ExprNode expression, StmtNode statement, int line, int column) {
+        super(line, column);
+        this.type = type;
+        this.identifier = identifier;
+        this.expression = expression;
+        this.statement = statement;
+    }
+
+    public TypeNode getType() {
+        return type;
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public ExprNode getExpression() {
+        return expression;
+    }
+
+    public StmtNode getStatement() {
+        return statement;
+    }
+
+    @Override
+    public String toString() {
+        return "{arrow(T x = e); S}";
+    }
+}
