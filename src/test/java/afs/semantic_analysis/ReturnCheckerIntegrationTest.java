ea7964a2a3d26543af784520a91d1950bacb6199
Jacob Højlund Simonsen <jacobhoejlund@gmail.com>
2025-05-27 13:32:22 +0200

    Add more return checker integration tests

--- src/test/java/afs/semantic_analysis/ReturnCheckerIntegrationTest.java
+++ src/test/java/afs/semantic_analysis/ReturnCheckerIntegrationTest.java
@@ -1,17 +1,19 @@
 package afs.semantic_analysis;
 
 import afs.nodes.prog.ProgNode;
+import afs.semantic_analysis.exceptions.MissingReturnException;
 import afs.semantic_analysis.exceptions.UnreachableCodeException;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 import setup.ASTGenerator;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ReturnCheckerIntegrationTest extends ReturnChecker {
 
     @Test
-    public void validReturns() {
+    public void functionReturns_ShouldNotThrow() {
+        // Arrange
         String input =
             "int t = 0;" +
             "fn int a() { " +
@@ -20,11 +22,17 @@ public class ReturnCheckerIntegrationTest extends ReturnChecker {
             "visualize a():" +
                 "t do a();";
         ProgNode prog = ASTGenerator.parseProgram(input);
-        checkReturn(prog);
+
+        // Act
+        Executable action = () -> checkReturn(prog);
+
+        // Assert
+        assertDoesNotThrow(action);
     }
 
     @Test
-    public void validReturnsComp() {
+    public void stmtCompReturnSecond_ShouldNotThrow() {
+        // Arrange
         String input =  "int t = 0;" +
                         "fn int a() { " +
                             "int x = 2;" +
@@ -33,11 +41,36 @@ public class ReturnCheckerIntegrationTest extends ReturnChecker {
                         "visualize a():" +
                             "t do a();";
         ProgNode prog = ASTGenerator.parseProgram(input);
-        checkReturn(prog);
+
+        // Act
+        Executable action = () -> checkReturn(prog);
+
+        // Assert
+        assertDoesNotThrow(action);
     }
 
     @Test
-    public void invalidReturnsComp() {
+    public void stmtCompReturnFirst_ShouldThrow() {
+        // Arrange
+        String input =  "int t = 0;" +
+                        "fn int a() { " +
+                            "return x;" +
+                            "int x = 2;" +
+                        "}"+
+                        "visualize a():" +
+                            "t do a();";
+        ProgNode prog = ASTGenerator.parseProgram(input);
+
+        // Act
+        Executable action = () -> checkReturn(prog);
+
+        // Assert
+        assertThrows(UnreachableCodeException.class, action, "Expected an UnreachableCodeException");
+    }
+
+    @Test
+    public void stmtCompBothReturn_ShouldThrow() {
+        // Arrange
         String input =  "int t = 0;\n" +
                 "fn int a() { \n" +
                     "return 2;\n" +
@@ -46,9 +79,142 @@ public class ReturnCheckerIntegrationTest extends ReturnChecker {
                 "visualize a():\n" +
                     "t do a();\n";
         ProgNode prog = ASTGenerator.parseProgram(input);
-        UnreachableCodeException exception = assertThrows(UnreachableCodeException.class, () -> {
-            checkReturn(prog);
-        });
-        assertEquals("Code after a return is unreachable: line 4, column 1", exception.getMessage());
+
+        // Act
+        Executable action = () -> checkReturn(prog);
+
+        // Assert
+        assertThrows(UnreachableCodeException.class, action, "Expected an UnreachableCodeException");
+    }
+
+    @Test
+    public void stmtIfBothReturn_ShouldNotThrow() {
+        // Arrange
+        String input =
+                "int t = 0;" +
+                "fn int a() { " +
+                    "if (true) then {" +
+                        "return 1;" +
+                    "} else {" +
+                        "return 2;" +
+                    "}" +
+                "}"+
+                "visualize a():" +
+                    "t do a();";
+        ProgNode prog = ASTGenerator.parseProgram(input);
+
+        // Act
+        Executable action = () -> checkReturn(prog);
+
+        // Assert
+        assertDoesNotThrow(action);
+    }
+
+    @Test
+    public void stmtIfMissingReturnAfter_ShouldThrow() {
+        // Arrange
+        String input =
+                "int t = 0;" +
+                "fn int a() { " +
+                    "if (true) then {" +
+                        "return 1;" +
+                    "}" +
+                "}"+
+                "visualize a():" +
+                "t do a();";
+        ProgNode prog = ASTGenerator.parseProgram(input);
+
+        // Act
+        Executable action = () -> checkReturn(prog);
+
+        // Assert
+        assertThrows(MissingReturnException.class, action, "Expected a MissingReturnException");
+    }
+
+    @Test
+    public void stmtIfMissingReturnInElse_ShouldThrow() {
+        // Arrange
+        String input =
+                "int t = 0;" +
+                "fn int a() { " +
+                    "if (true) then {" +
+                        "return 1;" +
+                    "} else {" +
+                    "}" +
+                "}"+
+                "visualize a():" +
+                "t do a();";
+        ProgNode prog = ASTGenerator.parseProgram(input);
+
+        // Act
+        Executable action = () -> checkReturn(prog);
+
+        // Assert
+        assertThrows(MissingReturnException.class, action, "Expected a MissingReturnException");
+    }
+
+    @Test
+    public void stmtIfMissingReturnInIf_ShouldThrow() {
+        // Arrange
+        String input =
+                "int t = 0;" +
+                "fn int a() { " +
+                    "if (true) then {" +
+                    "} else {" +
+                        "return 2;" +
+                    "}" +
+                "}" +
+                "visualize a():" +
+                "t do a();";
+        ProgNode prog = ASTGenerator.parseProgram(input);
+
+        // Act
+        Executable action = () -> checkReturn(prog);
+
+        // Assert
+        assertThrows(MissingReturnException.class, action, "Expected a MissingReturnException");
+    }
+
+    @Test
+    public void stmtIfMissingBothReturn_ShouldThrow() {
+        // Arrange
+        String input =
+                "int t = 0;" +
+                "fn int a() { " +
+                    "if (true) then {" +
+                    "} else {" +
+                    "}" +
+                "}" +
+                "visualize a():" +
+                    "t do a();";
+        ProgNode prog = ASTGenerator.parseProgram(input);
+
+        // Act
+        Executable action = () -> checkReturn(prog);
+
+        // Assert
+        assertThrows(MissingReturnException.class, action, "Expected a MissingReturnException");
+    }
+
+    @Test
+    public void stmtIfReturnAfter_ShouldNotThrow() {
+        // Arrange
+        String input =
+                "int t = 0;" +
+                "fn int a() { " +
+                    "if (true) then {" +
+                    "} else {" +
+                    "}" +
+                    "return 1;" +
+                "}" +
+                "visualize a():" +
+                    "t do a();";
+        ProgNode prog = ASTGenerator.parseProgram(input);
+
+        // Act
+        Executable action = () -> checkReturn(prog);
+
+        // Assert
+        assertDoesNotThrow(action);
     }
 }
